993b19f47ef889f4da64f819c68a9ec76693f6bc
2p02 <j.semik67@gmail.com>
2023-10-17 14:18:47 +0200

    ;)

---
+++ javascript/00 zadania/zadani 10/main.js
@@ -0,0 +1,10 @@
+const x = document.querySelector('#x')
+const btn = document.querySelector('button')
+const wynik = document.querySelector('#wynik')
+
+addEventListener('click',function (){
+    xx = parseInt(x.value)
+
+    let wzor =Math.pow(xx,2)
+
+} )
